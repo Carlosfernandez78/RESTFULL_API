c748a9c4e15aa5fca2a5ec5e2d5755c4c1f4b772
carlos fernandez <140237641+Carlosfernandez78@users.noreply.github.com>
2025-07-25 19:27:10 -0300

    eliminado

--- controllers/mainController.js
+++
@@ -1,3 +0,0 @@
-export const saludo = (req, res) => {
-  res.send('¡Hola Carlos, tu API Express está viva! ');
-};
--- routes/mainRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-
-router.post('/saludo', (req, res) => {
-  const { nombre } = req.body;
-  res.json({ mensaje: `¡Hola ${nombre}, bienvenido a la API!` });
-});
-
-export default router;
