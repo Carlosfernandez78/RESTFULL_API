4ffef2daf40c78b60284525fd885b201796afbec
carlos fernandez <140237641+Carlosfernandez78@users.noreply.github.com>
2025-08-01 14:35:54 -0300

    ento peroseguro

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // 1. Importar Express
-//import express from 'express';
+import express from 'express';
 
 // 2. Inicializar la app
-const express = require('express');
+//const express = require('express');
 const app = express();
 
 // Middleware global
